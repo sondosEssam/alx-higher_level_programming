f5093c36d3d6791310d05e83a7da980d4983d4af
SondosEssam <sondosessam713@gmail.com>
2024-03-12 16:49:34 +0200

    last digi by strings

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-s = str(abs(number))
-if abs(number) > 5:
-    print(f"Last digit of {number} is {s[-1]} and is greater than 5")
-elif abs(number) == 0:
-    print(f"Last digit of {number} is {s[-1]} and is 0")
-elif abs(number) < 5:
-    print(f"Last digit of {number} is {s[-1]} and is less than 6 and not 0")
+s = str((number))
+m = ""if number >= 0 else "-"
+r = (int(s[-1]))
+r = r * -1 if number < 0 else r * 1
+if r > 5:
+    print(f"Last digit of {number} is {m}{s[-1]} and is greater than 5")
+elif r == 0:
+    print(f"Last digit of {number} is {m}{s[-1]} and is 0")
+else:
+    print(f"Last digit of {number} is {m}{s[-1]} and is less than 6 and not 0")
