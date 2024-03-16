78f8aded6d9f76f9249bc9f10f1a61633cc434e7
SondosEssam <sondosessam713@gmail.com>
2024-03-16 17:28:53 +0200

    adding_the_None

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if not my_string:
+        return None
     listi = []
     for i in range(0, len(my_string)):
         if my_string[i] != 'c'and my_string[i] != 'C':
