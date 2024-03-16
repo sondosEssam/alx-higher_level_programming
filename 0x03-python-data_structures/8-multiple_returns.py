5b5110d8db96f292af8d3faeba57b7e890119081
SondosEssam <sondosessam713@gmail.com>
2024-03-16 11:36:37 +0200

    changin minor things

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == None:
-        a = (0, None)
+        a = None
         return a
     a = (len(sentence), sentence[0])
     return a
