b7bdb54dffb7f929c75db4f71c4c65e968907483
SondosEssam <sondosessam713@gmail.com>
2024-03-16 11:11:55 +0200

    pls work

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * connecting_through_back - make the linked list an array
+ * @head: head of the linked_list
+ * Return: int
+*/
+int *connecting_through_back(listint_t **head)
+{
+	listint_t *curr = *head, *curr2 = *head;
+	int n = 0, i = 1;
+
+	while (curr != NULL)
+	{
+		n++;
+		curr = curr->next;
+	}
+	int *p = malloc(sizeof(int) * (n + 1));
+
+	p[0] = n;
+	while (curr2 != NULL)
+	{
+		p[i] = curr2->n;
+		i++;
+		curr2 = curr2->next;
+	}
+	return (p);
+}
+/**
+ * is_palindrome - functon to check palindrome of the list
+ * @head: head of the linked list
+ * Return: int
+*/
+int is_palindrome(listint_t **head)
+{
+	int *p = connecting_through_back(head), *tmp = NULL;
+	int n = p[0] + 1, i = 0;
+
+	for (i = 1; i < n; i++)
+	{
+		if (p[i] != p[n - i])
+			return (0);
+	}
+	return (1);
+}
