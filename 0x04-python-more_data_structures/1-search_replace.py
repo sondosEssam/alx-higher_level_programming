886de5650725ff2348154750bb25ac5753132f84
SondosEssam <sondosessam713@gmail.com>
2024-03-21 17:41:04 +0200

    new listi

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return None
     new_listi = []
+    if not my_list:
+        return new_listi
     for j in range(len(my_list)):
         if my_list[j] == search:
             new_listi.append(replace)
