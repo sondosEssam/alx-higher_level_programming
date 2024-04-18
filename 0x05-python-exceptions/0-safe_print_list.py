fc0133bfc6cf7f966712d5312231c3ae305dc906
SondosEssam <sondosessam713@gmail.com>
2024-04-18 21:38:25 +0200

    no index error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     try:
         j = 0
-        for i in my_list:
-            print(i, end="")
+        for i in range(0, x):
+            print(my_list[i], end="")
             j += 1
         print()
         return j
     except IndexError as e:
         print()
+        return j
