08923ba78c7d932c3c6315f61c328a0f79096bec
SondosEssam <sondosessam713@gmail.com>
2024-04-20 19:38:42 +0200

    area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -22,7 +22,7 @@ class Square:
         Returns:
         float: area of the square
         """
-        return self.__size * 2
+        return self.__size * self.__size
     @property
     def size(self):
         """Getter for the size property.
