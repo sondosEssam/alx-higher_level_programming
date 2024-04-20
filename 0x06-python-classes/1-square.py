cd722de0ab1a1927c87f8df3757e59ae0909b415
SondosEssam <sondosessam713@gmail.com>
2024-04-20 14:18:00 +0200

    __size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+0-square.py omdule
+"""
+
+
+class Square:
+    """
+    spuare class documination
+    attributes:
+    __size: provte size value
+    """
+    def __init__(self, size):
+        """ __init__ constructor 
+        args:
+        size: size of the square
+        """
+        self.__size = size
+
