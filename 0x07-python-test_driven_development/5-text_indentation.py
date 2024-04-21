cbda4bbd73ef759aa28102bd6582e9caeacd92df
SondosEssam <sondosessam713@gmail.com>
2024-04-21 10:32:28 +0200

    gf

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -18,7 +18,7 @@ def text_indentation(text):
     i = 0
     while i < len(text):
         print(text[i], end="")
-        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+        if text[i] == '.' or text[i] == '?':
             print()
             print()
             while i + 1 < len(text) and \
