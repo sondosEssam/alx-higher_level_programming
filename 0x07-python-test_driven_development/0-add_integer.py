486ad8dd5d4712c54d09f04e88bfaa4fee76e1ac
SondosEssam <sondosessam713@gmail.com>
2024-04-21 06:59:01 +0200

    pythonfile

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+0-add_integer.py
+"""
+
+
+def add_integer(a, b=98):
+    """
+    add_integer
+    args:
+    a(int): 1st no
+    b(int): second no
+    Returns:
+    int
+    """
+    if not type(a) in (float, int):
+        raise TypeError("a must be an integer")
+    elif not type(b) in (float, int):
+        raise TypeError("b must be an integer")
+    a, b = int(a), int(b)
+    return a + b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
