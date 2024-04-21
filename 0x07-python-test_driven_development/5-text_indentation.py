aa812a174c7b69997bb283867a4637fd54bac2d7
SondosEssam <sondosessam713@gmail.com>
2024-04-21 10:31:15 +0200

    gf

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,14 +14,15 @@ def text_indentation(text):
     int
     """
     if not isinstance(text, str):
-        raise TypeError("stext must be a string")
+        raise TypeError("text must be a string")
     i = 0
     while i < len(text):
         print(text[i], end="")
-        if text[i] == '.' or text[i] == '?':
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
             print()
             print()
-            while i + 1 < len(text) and (text[i + 1] == ' ' or text[i - 1] == ' '):
+            while i + 1 < len(text) and \
+                    (text[i + 1] == ' ' or text[i - 1] == ' '):
                 i += 1
         i += 1
 
