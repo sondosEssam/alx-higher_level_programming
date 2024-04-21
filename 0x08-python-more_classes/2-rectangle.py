95fe2f71b539f0bdf6e0a047606b9f404eb53989
SondosEssam <sondosessam713@gmail.com>
2024-04-21 15:23:04 +0200

    area

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+0-rectangl.py module
+
+"""
+
+
+class Rectangle:
+    """
+    rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        """
+        __init method
+        args:
+        width: width
+        height: this is a height property with the getter and the setter
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        getter of the width property
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        getter of the heigh property
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """"
+        setter for the width property
+        args:
+        value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """"
+        setter for the width property
+        args:
+        value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        area of the rectangle
+        Returns:
+        float
+        """
+        return self.__width * self.__height
+    def perimeter(self):
+        """
+        perimeter of the rectangle
+        Returns:
+        int or float
+        """
+        return (self.__width + self.__height) * 2
