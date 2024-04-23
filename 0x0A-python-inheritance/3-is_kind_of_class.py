97103c027182885dd8cc558db982fa293fee66d5
SondosEssam <sondosessam713@gmail.com>
2024-04-24 00:56:52 +0200

    isinstance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+1-mylist module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    MyList
+    """
+    if isinstance(obj) is a_class:
+        return True
+    return False
