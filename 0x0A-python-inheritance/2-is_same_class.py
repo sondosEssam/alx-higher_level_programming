4996dd279ebe6cf7f18eaa0d924aae9d27b42ca1
SondosEssam <sondosessam713@gmail.com>
2024-04-24 00:53:45 +0200

    typeversersisinstance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+1-mylist module
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    MyList
+    """
+    if type(obj) is a_class:
+        return True
+    return False
