fee7934556356fc2efc73757b5212e4adb388803
SondosEssam <sondosessam713@gmail.com>
2024-04-24 01:32:16 +0200

    valid

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+1-mylist module
+"""
+
+
+class BaseGeometry:
+    """
+    MyList
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        if type(value) is not int:
+            raise TypeError(f"{name} must be greater an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
