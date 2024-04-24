f5ee558dce76c322e1026239f03ba41ccea719e3
SondosEssam <sondosessam713@gmail.com>
2024-04-25 01:23:29 +0200

    secondinherit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+1-mylist module
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """
+    rectangle
+    """
+    def __init__(self, size):
+        """
+        __init__
+        """
+        self.__size = super().integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """"
+        area
+        """
+        return int(self.__size) * int(self.__size)
