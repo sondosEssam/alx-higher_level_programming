7d3f2c416201e7600c89337a3e79dcd07ce6f79d
SondosEssam <sondosessam713@gmail.com>
2024-04-26 01:43:13 +0300

    read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""""
+0-read_file
+"""
+def read_file(filename=""):
+    """"
+    read file
+    """
+    with open(filename, encoding="UTF8") as f:
+        line = f.read()
+        print(line, end="")
