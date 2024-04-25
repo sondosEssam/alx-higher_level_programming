8e36b5ad4083ffb791916f6cedf1737b67722cc5
SondosEssam <sondosessam713@gmail.com>
2024-04-26 01:56:55 +0300

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,5 +9,6 @@ def write_file(filename="", text=""):
     read file
     """
     with open(filename, mode="w", encoding="UTF8") as f:
+        text = text[0:-1]
         f.write(text)
     return text
