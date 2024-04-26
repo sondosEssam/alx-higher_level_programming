267145104c4802a89d34964361856dfafa43e2ec
SondosEssam <sondosessam713@gmail.com>
2024-04-26 17:26:46 +0300

    none

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+documination
+"""
+
+
+class Student:
+    """
+    studnet
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """"
+        studnet
+        """
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def to_json(self, attrs=None):
+        """"
+        json
+        """
+        dicti = {}
+        if attrs is None:
+            return self.__dict__
+        for i in attrs:
+            if type(i) is str and self.__dict__.get(i) is not None:
+                dicti[i] = self.__dict__[i]
+        return dicti
