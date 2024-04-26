17f2a6702b5767b79fa972acacc0829e1fa310b5
SondosEssam <sondosessam713@gmail.com>
2024-04-26 03:28:52 +0300

    script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-""""
+"""
 0-read_file v.3
+
+This script reads command-line arguments, appends them to a list in memory,
+and then saves the updated list to a JSON file named "add_item.json".
+
+It assumes the existence of two other Python files:
+
+- 5-save_to_json_file.py: This file should contain a function named
+  `save_to_json_file` that takes a list and a filename as arguments and
+  saves the list to a JSON file.
+
+- 6-load_from_json_file.py: This file should contain a function named
+  `load_from_json_file` that takes a filename as an argument and loads a
+  list from the corresponding JSON file.
 """
 
 import sys
