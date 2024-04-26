d78a578d374d6185f3b7ec8b5d8576b28bd76ff0
SondosEssam <sondosessam713@gmail.com>
2024-04-26 05:05:06 +0300

    changes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
-"""
-7-add_item.py
-
-This script reads command-line arguments, appends them to a list in memory,
-and then saves the updated list to a JSON file named "add_item.json".
-
-It assumes the existence of two other Python files:
-
-- 5-save_to_json_file.py: This file should contain a function named
-  `save_to_json_file` that takes a list and a filename as arguments and
-  saves the list to a JSON file.
-
-- 6-load_from_json_file.py: This file should contain a function named
-  `load_from_json_file` that takes a filename as an argument and loads a
-  list from the corresponding JSON file.
+""""
+0-read_file v.3
 """
 
 import sys
-"""
-save json file
-"""
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-"""
-load json file
-"""
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file 
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-""""
-additional one
-"""
+
 
 filename = "add_item.json"
-with open(filename, mode="a", encoding="UTF8") as f:
+with open(filename, mode="w", encoding="UTF8") as f:
     arg = sys.argv[1:]
-    remin = load_from_json_file(filename)
-    for i in arg:
-        remin.append(i)
-    save_to_json_file(remin, filename)
+    save_to_json_file(arg, filename)
