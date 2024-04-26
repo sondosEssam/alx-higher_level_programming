ec30cb79cac8037d7ec839a879a5e73757ae5125
SondosEssam <sondosessam713@gmail.com>
2024-04-26 03:14:19 +0300

    new

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,6 +8,6 @@ def save_to_json_file(my_obj, filename):
     """"
     read file
     """
-    with open(filename, mode="w", encoding="UTF8") as f:
+    with open(filename, mode="a", encoding="UTF8") as f:
         import json
         json.dump(my_obj, f)
