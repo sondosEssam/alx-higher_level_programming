baa5ab028aa1b67eda0b3e0672dc23e14cb2b8ee
SondosEssam <sondosessam713@gmail.com>
2024-04-26 05:14:15 +0300

    usgaes:loads

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,11 @@
 0-read_file v.3
 """
 
+
+
 import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-
 filename = "add_item.json"
 with open(filename, mode="w", encoding="UTF8") as f:
     arg = sys.argv[1:]
