3c1bec45d27c771a8a99068d4f5659ab69a84553
SondosEssam <sondosessam713@gmail.com>
2024-04-26 21:49:36 +0300

    typeandvalueerrors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -55,6 +55,10 @@ class Rectangle(Base):
         """
         height setter
         """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @width.setter
@@ -62,6 +66,10 @@ class Rectangle(Base):
         """
         width setter
         """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @x.setter
@@ -69,6 +77,10 @@ class Rectangle(Base):
         """
         x setter
         """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
@@ -76,4 +88,8 @@ class Rectangle(Base):
         """
         y setter
         """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
