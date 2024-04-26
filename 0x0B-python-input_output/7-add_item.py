97d67c2b3629d9c969057f1d4dd85b9d5c90dc02
SondosEssam <sondosessam713@gmail.com>
2024-04-26 05:23:56 +0300

    usgaes:loads

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-"""
-0-read_file v.3
-"""
-
-
-import sys
-
-
+"""Script that adds all arguments to a Python list, and then saves them
+to a file"""
+from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
@@ -17,8 +12,8 @@ def add_item():
   """
   filename = "add_item.json"
   with open(filename, mode="w", encoding="UTF8") as f:
-    arg = sys.argv[1:]
-    r = load_from_json_file(sys.argv[0])
+    arg = argv[1:]
+    r = load_from_json_file(argv[0])
     for i in arg:
         r.append(i)
   save_to_json_file(r, filename)
