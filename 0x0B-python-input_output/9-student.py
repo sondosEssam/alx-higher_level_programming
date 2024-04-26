07dc240d0386f1ee34fa0e51197c0a50184198f4
SondosEssam <sondosessam713@gmail.com>
2024-04-26 15:18:27 +0300

    calss

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+documination
+"""
+
+
+class Student:
+    """
+    studnet
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """"
+        studnet
+        """
+        self.age = age
+        self.first_name =first_name
+        self.last_name = last_name
+
+    def to_json(self):
+        """"
+        json
+        """
+        return self.__dict__
