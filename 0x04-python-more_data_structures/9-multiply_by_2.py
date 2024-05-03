a0957e9786a18edf666eeb7fcb9d89397112e234
SondosEssam <sondosessam713@gmail.com>
2024-05-03 14:47:18 +0300

    edit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,7 @@
 def multiply_by_2(a_dictionary):
     if a_dictionary is None:
         return a_dictionary
-    for i in a_dictionary.keys():
-        a_dictionary[i] *= 2
-    return a_dictionary
+    new_dict = a_dictionary.copy()
+    for i in new_dict.keys():
+        new_dict[i] *= 2
+    return new_dict
