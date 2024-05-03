02371a65003a8fc4b1693e927085c6052daafc1c
SondosEssam <sondosessam713@gmail.com>
2024-05-03 14:52:52 +0300

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return a_dictionary
+        return None
     new_dict = -100
     str = ""
     for i, j in a_dictionary.items():
