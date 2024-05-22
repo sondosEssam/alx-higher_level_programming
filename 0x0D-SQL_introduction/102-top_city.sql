541865ff9eac937e0755cb666b9ce4db50380113
SondosEssam <sondosessam713@gmail.com>
2024-05-22 05:09:54 +0300

    again

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 or month = 8 GROUP BY city ORDER BY AVG(value) DESC ;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 or month = 8 GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
