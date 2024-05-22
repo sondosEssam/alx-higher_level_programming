69294abf1fada4abca6293a68823957471f79f3d
SondosEssam <sondosessam713@gmail.com>
2024-05-23 01:28:41 +0300

    new

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 -- second commetn
 
 USE hbtn_0d_usa;
-SELECT cities.id , cities.name FROM cities RIGHT JOIN states ON cities.state_id = states.id WHERE states.name = "California"
+SELECT cities.id , cities.name FROM cities RIGHT JOIN states ON cities.state_id = states.id WHERE states.name = "California" ORDER BY cities.id ASC;
