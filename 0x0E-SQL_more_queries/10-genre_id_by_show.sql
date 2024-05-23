1b0e8333a7e36a26a3571dee8b2369d54d31b34b
SondosEssam <sondosessam713@gmail.com>
2024-05-23 15:27:01 +0300

    complex

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- first file
+-- second commetn
+SELECT tv_shows.title , tv_show_genres.genre_id FROM tv_shows , tv_show_genres WHERE tv_shows.id = tv_show_genres.show_id AND tv_show_genres.genre_id > 0 ORDER BY tv_shows.title , tv_show_genres.genre_id ASC
