70a8fc58fa37405ecdc481da8ec35209c46d4c0f
SondosEssam <sondosessam713@gmail.com>
2024-06-19 04:03:19 +0300

    cxc

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+const firstArg = args[0];
+const secondArg = args[1];
+function add(a, b){
+console.log(Number(firstArg) + Number(secondArg));
+}
+add(firstArg, secondArg);
