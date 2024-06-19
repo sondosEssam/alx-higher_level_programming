1bee9f1cde7bd1536fe8952bac8f5e756b2ca345
SondosEssam <sondosessam713@gmail.com>
2024-06-19 03:50:46 +0300

    for

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const con = [];
+con.push('C is fun');
+con.push('Python is cool');
+con.push('JavaScript is amazing');
+for (let i = 0; i < con.length; i += 1) {
+  console.log(con[i]);
+}
