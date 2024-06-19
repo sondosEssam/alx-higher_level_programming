eda6288fcdb93b7abb2f84f87ba7f90a38ac056a
SondosEssam <sondosessam713@gmail.com>
2024-06-20 00:05:18 +0300

    sq

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const square = require("./5-square.js");
+const square = require('./5-square.js');
 class Square extends square {
-
   charPrint (c) {
     for (let i = 0; i < this.height; i += 1) {
       if (c === undefined) { console.log('X'.repeat(this.width)); } else { console.log(c.repeat(this.width)); }
