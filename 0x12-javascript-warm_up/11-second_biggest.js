4eae3c081c27aaab96fdd75c4163ffbb47a517e0
SondosEssam <sondosessam713@gmail.com>
2024-06-19 19:54:44 +0300

    fd

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@ const args = process.argv.slice(2);
 if (args.length <= 1) {
   console.log(0);
 } else {
-  const args2 = args.sort();
-  console.log(args2[args2.length - 2]);
+  const args2 = args.map(Number);
+  const args3 = args2.sort((a, b) => a - b);
+  console.log(args3[args3.length - 2]);
 }
