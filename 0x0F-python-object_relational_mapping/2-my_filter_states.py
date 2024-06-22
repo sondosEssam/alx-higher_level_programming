04f7f02d199b90baf6cb4da0b62edb668be695b8
SondosEssam <sondosessam713@gmail.com>
2024-06-22 16:06:17 +0300

    BINARY

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         with d.Connect(user=a[1], passwd=a[2], db=a[3], port=3306) as db:
             with db.cursor() as cursor:
                 query = """SELECT * FROM states WHERE name
-                = '{}' ORDER BY id""".format(a[4])
+                LIKE BINARY '{}' ORDER BY id""".format(a[4])
                 cursor.execute(query)
                 rows = cursor.fetchall()
                 for row in rows:
