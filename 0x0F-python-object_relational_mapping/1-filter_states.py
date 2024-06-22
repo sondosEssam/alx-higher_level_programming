b45eb003c23f68effe72382b3311c13de88d6482
SondosEssam <sondosessam713@gmail.com>
2024-06-22 15:54:09 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,16 +2,15 @@
 """
 comment
 """
-if __name__ == "__main__":
-    import MySQLdb as d
-    from sys import argv as a
-    try:
-        with d.Connect(user=a[1], passwd=a[2], db=a[3], port=3306) as db:
-            with db.cursor() as cursor:
-                query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
-                cursor.execute(query)
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(row)
-    except Exception as e:
-        print(e)
+import MySQLdb as d
+from sys import argv as a
+try:
+    with d.Connect(user=a[1], passwd=a[2], db=a[3], port=3306) as db:
+        with db.cursor() as cursor:
+            query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+except Exception as e:
+    print(e)
