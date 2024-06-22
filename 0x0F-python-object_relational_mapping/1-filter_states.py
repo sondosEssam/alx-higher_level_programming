44d78db3fc0e5459c2f57e60708187a7e79c5b6c
SondosEssam <sondosessam713@gmail.com>
2024-06-22 15:57:47 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,8 @@ from sys import argv as a
 try:
     with d.Connect(user=a[1], passwd=a[2], db=a[3], port=3306) as db:
         with db.cursor() as cursor:
-            query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+            query = """SELECT * FROM states WHERE name
+            LIKE BINARY 'N%' ORDER BY id"""
             cursor.execute(query)
             rows = cursor.fetchall()
             for row in rows:
