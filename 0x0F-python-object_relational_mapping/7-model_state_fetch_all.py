69fcea73578ea306589e1f36ae788af8a6c4855f
SondosEssam <sondosessam713@gmail.com>
2024-06-23 16:37:26 +0300

    fetch

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv as a
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqldb://{a[1]}:{a[2]}@localhost/{a[3]}",
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    sataes = session.query(State).all()
+    for state in sataes:
+        print(f"{state.id}: {state.name}")
