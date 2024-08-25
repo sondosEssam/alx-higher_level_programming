eb7824de1f3757b01d4b4b6b112b42163c950668
SondosEssam <sondosessam713@gmail.com>
2024-08-25 19:25:32 +0300

    post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #commment
-curl -s -d "email: test@gmail.com&subject: I will always be here for PLD" -X "POST" "$1"
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -X "POST" "$1"
